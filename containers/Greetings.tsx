06e09d14e9004d9137d5025934a41a170cb0c026
rehmat11872 <raorehmat11@gmail.com>
2025-05-15 16:07:06 +0500

    fixing isssue

--- containers/Greetings.tsx
+++ containers/Greetings.tsx
@@ -50,7 +50,7 @@ const Greetings = () => {
                   )}
                 </Col>
                 <Col lg="6">
-                  <GreetingLottie animationPath="/lottie/coding.json" />
+                  <GreetingLottie/>
                 </Col>
               </Row>
             </div>
